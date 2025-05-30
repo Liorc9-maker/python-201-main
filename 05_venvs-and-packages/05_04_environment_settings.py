657650a71bb7ec01c9e7fc9c36dd04f7cc5fd92c
Lior Cohen <liorcohen990@gmail,com>
2025-05-30 01:58:02 +0000

    30/5

--- 05_venvs-and-packages/05_04_environment_settings.py
+++ 05_venvs-and-packages/05_04_environment_settings.py
@@ -6,3 +6,9 @@
 # 
 # Then write the necessary code to access and print the values of these
 # two environment variables in this script.
+
+import os
+
+var1 = os.environ['ENVIRONMENT']
+var2 = os.environ['SECRET']
+print(f"{var1}\n{var2}")
